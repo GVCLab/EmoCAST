5a2a690676845a96168eff0982381e36414ac841
mao <59043661+Jiang-maomao@users.noreply.github.com>
2025-08-27 16:55:51 +0800

    Add files via upload

---
+++ inference.sh
@@ -0,0 +1,3 @@
+python scripts/inference.py  --source_image ./examples/reference_images/0007.png \
+                          --driving_audio ./examples/driving_audios/vox2_neu_004.wav \
+                            --prompt_emo "The portrait is experiencing chronic illness or pain."
